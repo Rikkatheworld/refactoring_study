676f482cf52ae0c14a7b70208ed9574e4bfac675
rikkaxie <rikkaxie@futunn.com>
2021-11-30 16:09:06 +0800

    优化第一版代码

--- src/CircusDemo.kt
+++ src/CircusDemo.kt
@@ -1,6 +1,7 @@
 import kotlin.math.floor
 
 data class PlayInfo(val name: String, val type: String)
+
 val playInfos: MutableMap<String, PlayInfo> = mutableMapOf(
     Pair("西红市首富", PlayInfo("西红市首富", "comedy")),
     Pair("夏洛的烦恼", PlayInfo("夏洛的烦恼", "comedy")),
@@ -9,6 +10,7 @@ val playInfos: MutableMap<String, PlayInfo> = mutableMapOf(
 
 data class PerformancesData(val playId: String, val audience: Int)
 data class Invoices(val customer: String, val performances: List<PerformancesData>)
+
 val invoices = Invoices(
     "中船戏院", listOf(
         PerformancesData("西红市首富", 55),
@@ -17,40 +19,71 @@ val invoices = Invoices(
     )
 )
 
-fun statement(invoice: Invoices, plays: MutableMap<String, PlayInfo>): String {
-    var totalAmount = 0
-    var volumeCredits = 0
-    var result = "这里您的账单: ${invoice.customer} \n"
+/**
+ * 用来计算一次账单的工具
+ */
+class Statement(private val invoice: Invoices, private val plays: MutableMap<String, PlayInfo>) {
 
-    for (perf in invoice.performances) {
-        val playInfo = plays[perf.playId] ?: continue
-        var thisAmount = 0
+    fun getStatement(): String {
+        var result = "这里您的账单: ${invoice.customer} \n"
+        for (perf in invoice.performances) {
+            result += " ${playFor(perf).name}: ${amountFor(perf) / 100f} (${perf.audience} 观众) \n "
+        }
 
-        when (playInfo.type) {
+        result += "所需缴费：￥${totalAmount(invoice) / 100f}\n"
+        result += "你获取了 ${totalVolumeCredits(invoice)} 信用分 \n"
+        return result
+    }
+
+    private fun totalAmount(invoice: Invoices): Int {
+        var result = 0
+        for (perf in invoice.performances) {
+            result += amountFor(perf)
+        }
+        return result
+    }
+
+    private fun totalVolumeCredits(invoice: Invoices): Int {
+        var result = 0
+        for (perf in invoice.performances) {
+            result += volumeCreditsFor(perf)
+        }
+        return result
+    }
+
+    private fun volumeCreditsFor(perf: PerformancesData): Int {
+        var result = 0
+        result += (perf.audience - 30).coerceAtLeast(0)
+        if (playFor(perf).type == "comedy") result += floor(perf.audience / 5f).toInt()
+        return result
+    }
+
+    private fun amountFor(perf: PerformancesData): Int {
+        var result: Int
+        when (playFor(perf).type) {
             "tragedy" -> {
-                thisAmount = 40000
+                result = 40000
                 if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30)
+                    result += 1000 * (perf.audience - 30)
                 }
             }
             "comedy" -> {
-                thisAmount = 30000
+                result = 30000
                 if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20)
+                    result += 10000 + 500 * (perf.audience - 20)
                 }
-                thisAmount += 300 * perf.audience
+                result += 300 * perf.audience
             }
+            else -> throw Exception("unknown type:${playFor(perf).type}")
         }
-        volumeCredits += (perf.audience - 30).coerceAtLeast(0)
-        if (playInfo.type == "comedy") volumeCredits += floor(perf.audience / 5f).toInt()
-        result += " ${playInfo.name}: ${thisAmount / 100f} (${perf.audience} 观众) \n "
-        totalAmount += thisAmount
+        return result
     }
-    result += "所欠金额：￥${totalAmount / 100f}\n"
-    result += "你获取了 $volumeCredits 信用分 \n"
-    return result
+
+    private fun playFor(perf: PerformancesData): PlayInfo =
+        plays[perf.playId] ?: PlayInfo("", "")
 }
 
 fun main() {
-    print(statement(invoices, playInfos))
+    val statement = Statement(invoices, playInfos)
+    print(statement.getStatement())
 }
